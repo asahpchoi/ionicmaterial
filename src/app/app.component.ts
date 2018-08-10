dbcebfc7ce07b41f2267380c88c8ba556b8376d1
dev <dev@atos-MacBook-Pro.local>
2018-08-10 16:12:51 +0800

    X

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { fadeAnimation } from './router.animations';
 
 @Component({
   selector: 'app-root',
+  animations: [fadeAnimation],
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
@@ -23,4 +25,10 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
+  getState(outlet) {
+ 
+    return outlet.activatedRouteData.state;
+  }
+
 }
